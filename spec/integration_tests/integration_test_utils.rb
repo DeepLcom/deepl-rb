77ca2bad7176bca01a7c24fb3e8b1aad2b735691
Jan Ebbing <Jan.Ebbing@deepl.com>
2024-09-13 15:17:56 +0100

    chore: Add copyright header

--- spec/integration_tests/integration_test_utils.rb
+++ spec/integration_tests/integration_test_utils.rb
@@ -1,3 +1,6 @@
+# Copyright 2024 DeepL SE (https://www.deepl.com)
+# Use of this source code is governed by an MIT
+# license that can be found in the LICENSE file.
 # frozen_string_literal: true
 
 require 'securerandom'
