22162a85a04e4a39c8c2ffe20faf839727410478
Jan Ebbing <Jan.Ebbing@deepl.com>
2024-09-17 15:21:05 +0100

    chore: Update copyright headers

--- lib/deepl/requests/languages.rb
+++ lib/deepl/requests/languages.rb
@@ -1,4 +1,4 @@
-# Copyright 2021 Daniel Herzog
+# Copyright 2024 DeepL SE
 # Use of this source code is governed by an MIT
 # license that can be found in the LICENSE.md file.
 # frozen_string_literal: true
